2e04a68c261a6606f493096b7a686f2473f7fbcd
sdubey6 <sdubey6@uic.edu>
2017-08-02 14:19:37 -0400

    Update test_hi.py

--- test_hi.py
+++ test_hi.py
@@ -6,10 +6,9 @@ from selenium import webdriver
 
 @pytest.yield_fixture
 def driver():
-    desired_cap= {'browser': 'Chrome', 'browser_version': '59.0', 'os': 'Windows', 'os_version': '10', 'resolution': '1024x768'}
-    driver= webdriver.Remote(
-    command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
-    desired_capabilities= desired_cap)
+    driver = webdriver.Remote(
+  command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
+  desired_capabilities=DesiredCapabilities.FIREFOX)
     yield driver
     driver.quit()
     
