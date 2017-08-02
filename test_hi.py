2797cab21f4c0ec29bfc2eacf8d47af9a05e15bf
sdubey6 <sdubey6@uic.edu>
2017-08-02 14:23:09 -0400

    Update test_hi.py

--- test_hi.py
+++ test_hi.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 
 @pytest.yield_fixture
 def driver():
+    desired_cap = {'browser': 'Firefox', 'browser_version': '55.0 beta', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1024x768'}
     driver = webdriver.Remote(command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
     desired_capabilities=DesiredCapabilities.FIREFOX)
     yield driver
