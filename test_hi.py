b64811b2748742ced8d2316f00de0dc1a1c64e83
sdubey6 <sdubey6@uic.edu>
2017-08-02 14:21:40 -0400

    Update test_hi.py

--- test_hi.py
+++ test_hi.py
@@ -6,9 +6,8 @@ from selenium import webdriver
 
 @pytest.yield_fixture
 def driver():
-    driver = webdriver.Remote(
-  command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
-  desired_capabilities=DesiredCapabilities.FIREFOX)
+    driver = webdriver.Remote(command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
+    desired_capabilities=DesiredCapabilities.FIREFOX)
     yield driver
     driver.quit()
     
