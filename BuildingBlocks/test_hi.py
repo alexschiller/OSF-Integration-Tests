26975abc930201dc2add102bd25e3051a03ebcfe
sdubey6 <sdubey6@uic.edu>
2017-08-03 13:21:09 -0400

    added browser capability

---
+++ BuildingBlocks/test_hi.py
@@ -0,0 +1,17 @@
+from Login import Login
+import pytest
+import time
+from selenium import webdriver
+
+
+@pytest.yield_fixture
+def driver():
+    desired_cap = {'browser': 'Safari', 'browser_version': '10.1', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1024x768'}
+    driver= webdriver.Remote(
+    command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
+    desired_capabilities= desired_cap)
+    yield driver
+    driver.quit()
+    
+def test_login(driver):
+  Login(driver)
