b9efaaea2bf186a8f46ee95ac6f1562f3f4f813a
sdubey6 <sdubey6@uic.edu>
2017-08-03 15:12:41 -0400

    Update test_hi.py

--- BuildingBlocks/test_hi.py
+++ BuildingBlocks/test_hi.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 
 @pytest.yield_fixture
 def driver():
-    desired_cap = {'browser': 'Safari', 'browser_version': '10.1', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1024x768'}
+    desired_cap = {'browser': 'Chrome', 'browser_version': '60.0', 'os': 'Windows', 'os_version': '10', 'resolution': '1024x768'}
     driver= webdriver.Remote(
     command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
     desired_capabilities= desired_cap)
