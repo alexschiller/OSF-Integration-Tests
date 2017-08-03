700cadb2fb963c214722288a3f1db669c08c500c
sdubey6 <sdubey6@uic.edu>
2017-08-03 15:59:48 -0400

    Update test_hi.py

--- BuildingBlocks/test_hi.py
+++ BuildingBlocks/test_hi.py
@@ -1,4 +1,5 @@
 from Login import Login
+from CreateProject import CreateProject
 import pytest
 import time
 from selenium import webdriver
@@ -6,11 +7,13 @@ from selenium import webdriver
 
 @pytest.yield_fixture
 def driver():
-    desired_cap = {'browser': 'Firefox', 'browser_version': '55.0 beta', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1024x768'}
-    driver = webdriver.Remote(command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
-    desired_capabilities=desired_cap)
+    desired_cap = {'browser': 'Firefox', 'browser_version': '55.0 beta', 'os': 'OS X', 'os_version': 'Sierra', 'resolution': '1920x1080'}
+    driver= webdriver.Remote(
+    command_executor='http://shikhadubey1:Mhtt1XkQq18k8nqQzsqn@hub.browserstack.com:80/wd/hub',
+    desired_capabilities= desired_cap)
     yield driver
     driver.quit()
     
-def test_login(driver):
-  Login(driver)
+def test_yes(driver):
+    Login(driver)
+    CreateProject(driver)
