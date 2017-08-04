5f01c425bb99c01c3a18665f2c4a146059f218aa
sdubey6 <sdubey6@uic.edu>
2017-08-04 09:18:12 -0400

    Update Login.py

--- BuildingBlocks/Login.py
+++ BuildingBlocks/Login.py
@@ -1,8 +1,4 @@
-'''
-Created on Jun 13, 2017
 
-@author: shikhadubey
-'''
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
